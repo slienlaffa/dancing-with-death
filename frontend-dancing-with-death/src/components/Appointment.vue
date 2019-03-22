f4fe6b0ce4bec0af579d6e86b64eba327a2c2f84
Sergio Lienlaff Aguirre <s.lienlaff.a@gmail.com>
2019-03-22 00:12:10 -0300

    dark-mode+some-images+calendar

--- frontend-dancing-with-death/src/components/Appointment.vue
+++ frontend-dancing-with-death/src/components/Appointment.vue
@@ -1,52 +1,29 @@
 <template>
   <div class="container">
-    <h1 class="title is-1">Dancing with Death</h1>
+    <h1 class="title is-1">
+      Dancing with Death
+      <img src="../assets/death-icon.png" alt="death icon" id="logo" />
+    </h1>
     <div class="columns">
-      <div class="column is-one-quarter">
+      <div class="column is-one-third">
         <form method="POST" @submit.prevent="addItem()">
           <div class="field">
             <label class="label">Date Appointment</label>
-            <div class="control">
-              <input
-                class="input"
-                type="date"
-                v-model="DateAppointment"
-                @change="fetchAvailableHours()"
-                required
+            <div class="control" id="calendar-wrap">
+              <Calendar
+                ref="calendar"
+                :sundayBegin="false"
+                @date="setDate($event)"
               />
-            </div>
-          </div>
-          <div class="card">
-            <div class="field">
-              <div class="card-header">
-                <p class="card-header-title">Select an hour</p>
-              </div>
-              <div class="card-content">
-                <div v-show="DateAppointment" class="control">
-                  <p v-if="!radios.length">All hours are already booked</p>
-                  <label
-                    v-else
-                    class="radio"
-                    v-for="(radio, key) in radios"
-                    :key="key"
-                  >
-                    <input
-                      class="radio"
-                      type="radio"
-                      v-model="StartTime"
-                      :value="radio"
-                      required
-                    />
-                    At {{ radio }}:00
-                  </label>
-                </div>
-              </div>
             </div>
           </div>
 
           <div class="field">
             <label class="label">Email</label>
-            <div class="control">
+            <div class="control has-icons-left">
+              <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+              </span>
               <input
                 class="input"
                 type="email"
@@ -57,7 +34,10 @@
           </div>
           <div class="field">
             <label class="label">Phone</label>
-            <div class="control">
+            <div class="control has-icons-left">
+              <span class="icon is-small is-left">
+                <i class="fas fa-phone"></i>
+              </span>
               <input
                 class="input"
                 type="text"
@@ -69,8 +49,40 @@
           <div class="field">
             <button type="submit" class="button is-link">Book</button>
           </div>
-          <p class="help is-danger is-size-5">{{ error }}</p>
+          <ul class="help is-danger is-size-8">
+            <li v-for="(error, key) in errors" :key="key">{{ error }}</li>
+          </ul>
         </form>
+      </div>
+      <div class="column is-one-fifth">
+        <div class="card" id="hours">
+          <div class="field">
+            <div class="card-header">
+              <p class="card-header-title">Select an hour</p>
+            </div>
+            <div class="card-content" style="padding:0.5rem;">
+              <div v-show="DateAppointment" class="control">
+                <p v-if="!radios.length">All hours are already booked</p>
+                <label
+                  v-else
+                  class="radio"
+                  v-for="(radio, key) in radios"
+                  :key="key"
+                >
+                  <input
+                    class="radio"
+                    type="radio"
+                    v-model="StartTime"
+                    name="StartTime"
+                    :value="radio"
+                    required
+                  />
+                  At {{ radio }}:00
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div class="column">
@@ -90,8 +102,8 @@
             <tr v-for="(item, key) in appointments" :key="key">
               <td>{{ new Date(item.dateAppointment).toLocaleDateString() }}</td>
               <td>{{ item.startTime }}:00</td>
-              <td>{{ item.email ? item.email : '' }}</td>
-              <td>{{ item.phoneNumber ? item.phoneNumber : '' }}</td>
+              <td>{{ item.email }}</td>
+              <td>{{ item.phoneNumber }}</td>
               <td></td>
             </tr>
           </tbody>
@@ -104,14 +116,17 @@
 <script>
 // TODO mensaje de existo al poster
 import axios from 'axios'
-import 'bulma/css/bulma.css'
+import Calendar from 'imarker'
 
 export default {
   name: 'Appointment',
+  components: {
+    Calendar
+  },
   data() {
     return {
       uri: 'https://localhost:5001/api/appointments',
-      error: '',
+      errors: [],
       appointments: [],
       Email: null,
       radios: [],
@@ -121,34 +136,46 @@ export default {
     }
   },
   methods: {
-    async fetchAvailableHours() {
-      this.error = ''
+    async fetchAvailableHours() {},
+    async addItem() {
+      this.errors = []
+      if (
+        this.DateAppointment &&
+        this.StartTime &&
+        (this.Email || this.PhoneNumber)
+      ) {
+        const appointment = {
+          DateAppointment: this.DateAppointment,
+          StartTime: this.StartTime,
+          Email: this.Email,
+          PhoneNumber: this.PhoneNumber
+        }
+        try {
+          const response = await axios.post(this.uri, appointment)
+          if (this.appointments.length >= 10) this.appointments.pop()
+          this.appointments.unshift(response.data)
+          this.Email = null
+          this.PhoneNumber = null
+        } catch (error) {
+          this.errors.push(error.response.data)
+        }
+      } else {
+        if (!this.DateAppointment) this.errors.push('Date required')
+        if (!this.StartTime) this.errors.push('StartTime required')
+        if (!this.Email && !this.PhoneNumber)
+          this.errors.push('Contact information required(EMAIL or PHONE)')
+      }
+    },
+    async setDate(e) {
+      this.DateAppointment = e.date
+      this.errors = []
       try {
         const response = await axios.get(
           this.uri + '/getAvailableHours/' + this.DateAppointment
         )
         this.radios = response.data
       } catch (error) {
-        this.error = error.response.data
-      }
-    },
-    async addItem() {
-      this.error = ''
-      const appointment = {
-        DateAppointment: this.DateAppointment,
-        StartTime: this.StartTime,
-        Email: this.Email,
-        PhoneNumber: this.PhoneNumber
-      }
-      try {
-        const response = await axios.post(this.uri, appointment)
-        if (this.appointments.length >= 10) this.appointments.pop()
-        this.appointments.unshift(response.data)
-        this.Email = null
-        this.PhoneNumber = null
-        this.fetchAvailableHours()
-      } catch (error) {
-        this.error = error.response.data
+        this.errors.push(error.response.data)
       }
     }
   },
@@ -157,10 +184,27 @@ export default {
       const response = await axios.get(this.uri)
       this.appointments = response.data
     } catch (error) {
-      this.error = error
+      this.errors.push(error)
     }
   }
 }
 </script>
 
-<style></style>
+<style>
+#calendar-wrap #calendar {
+  min-width: 200px;
+  margin-bottom: 10px;
+}
+#calendar-wrap .weekend-day span {
+  color: #ee2e48;
+}
+#hours {
+  margin-top: 30px;
+}
+.radio {
+  display: table-row;
+}
+#logo {
+  margin-top: 5px;
+}
+</style>
